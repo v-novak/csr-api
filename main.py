38a01586dde586470c23358434a9110752152417
Vasiliy Novak <vnovak2048@gmail.com>
2021-11-15 03:34:58 +0300

    change /api url

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import settings
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('{}')
+        self.write('{"laws": ["A robot may not injure a human being or, through inaction, allow a human being to come to harm", "A robot must obey the orders given it by human beings except where such orders would conflict with the First Law", "A robot must protect its own existence as long as such protection does not conflict with the First or Second Law"]}')
 
 def make_app(settings={}):
-    return tornado.web.Application([(r'/', MainHandler)], **settings)
+    return tornado.web.Application([(r'/api', MainHandler)], **settings)
 
 if __name__ == '__main__':
     app = make_app(settings.server_settings())
